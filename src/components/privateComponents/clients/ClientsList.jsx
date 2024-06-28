b62cdfe1d0033a213fcb8d5c23acbb1c76de7e8c
Alessandro1809 <dalessandro18dg@gmail.com>
2024-06-28 17:21:07 -0600

    agregando recorrido de clientes para poder usar la API

---
+++ src/components/privateComponents/clients/ClientsList.jsx
@@ -0,0 +1,30 @@
+ import { useState, useEffect } from "react";
+ import clienteAxios from "../../../config/axios";
+ const ClientsList = () => {
+
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+      const getClients = async () => {
+        const response = await clienteAxios("/api/cliente");
+        const data = await response.json();
+        setClients(data);
+      }
+      getClients();
+    }, []);
+
+
+  return (
+    <>
+        {clients && clients.map(client => (
+            <div key={client._id}>
+                <p className="text-white text-center">{client.name}</p>
+            </div>
+        ))
+
+    }
+    </>
+  )
+}
+
+export default ClientsList;
